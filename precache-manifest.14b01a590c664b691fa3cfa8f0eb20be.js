5a740320ff6313e019ac7eda8f7a46d802eb99d3
Johnson Mwakazi <mwanjilajohnson@gmail.com>
2020-07-16 09:40:40 +0300

    Updates

---
+++ precache-manifest.14b01a590c664b691fa3cfa8f0eb20be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1938a92e143d9a9a340dcaaf385ca6b4",
+    "url": "/monsters-rolodexx/index.html"
+  },
+  {
+    "revision": "26d6ada6fb33a7abe06a",
+    "url": "/monsters-rolodexx/static/css/main.eac74443.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodexx/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodexx/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d6ada6fb33a7abe06a",
+    "url": "/monsters-rolodexx/static/js/main.50b09303.chunk.js"
+  },
+  {
+    "revision": "efdcc714552c7dfffd68",
+    "url": "/monsters-rolodexx/static/js/runtime-main.e2d810c5.js"
+  }
+]);
